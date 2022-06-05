54b95789b6623ecdd8986a31130a1d98c5918132
Никита Лазюк <nlazyuk@MacBook-Pro-Nikita.local>
2022-06-05 18:53:38 +0500

    recursive count and max

--- recursion/recursion.go
+++ recursion/recursion.go
@@ -5,26 +5,58 @@ import "fmt"
 func main() {
 	factNumber := 5
 	fact := fact(factNumber)
-	fmt.Printf("factorial on %d is %d\n", factNumber, fact)
+	fmt.Printf("factorial for %d is %d\n", factNumber, fact)
 
-	s := [] int {3,2,8}
+	s := []int{11}
+
 	sum := sum(s)
-	fmt.Printf("sum on %v is %d\n", s, sum)
+	fmt.Printf("sum for %v is %d\n", s, sum)
+
+	count := count(s)
+	fmt.Printf("count for %v is %d\n", s, count)
+
+	max := max(s)
+	fmt.Printf("max for %v is %d\n", s, max)
 
 }
 
 func fact(x int) int {
 	if x == 1 {
 		return x
-	} else {
-		return x * fact(x-1)
 	}
+	return x * fact(x-1)
+
 }
 
-func sum (s []int) int {
-	if(len(s) == 0) {
+func sum(s []int) int {
+	if len(s) == 0 {
 		return 0
-	}else {
-		return s[len(s)-1] + sum(s[0:len(s)-1])
 	}
+	return s[len(s)-1] + sum(s[0:len(s)-1])
+}
+
+func count(s []int) int {
+	if len(s) == 0 {
+		return 0
+	}
+	return 1 + count(s[0:len(s)-1])
+}
+
+func max(s []int) int {
+	if len(s) == 1 {
+		return s[0]
+	}
+	if len(s) == 2 {
+		if s[0] > s[1] {
+			return s[0]
+		} else {
+			return s[1]
+		}
+	}
+	max := max(s[0 : len(s)-1])
+	if s[len(s)-1] > max {
+		return s[len(s)-1]
+	}
+
+	return max
 }
