8f9cc7ccf61145b5c26cd3a0ac23a56ad6371247
Никита Лазюк <nlazyuk@MacBook-Pro-Nikita.local>
2022-06-05 16:06:36 +0500

    add recursion slice sum

--- recursion/recursion.go
+++ recursion/recursion.go
@@ -1,22 +1,30 @@
-package main 
+package main
 
 import "fmt"
 
-
 func main() {
-	var number int
-	fmt.Println("Enter number for factorial...")
-	fmt.Scanf("%d\n", &number)
+	factNumber := 5
+	fact := fact(factNumber)
+	fmt.Printf("factorial on %d is %d\n", factNumber, fact)
 
-	fact := fact(number)
-	fmt.Println(fact)
+	s := [] int {3,2,8}
+	sum := sum(s)
+	fmt.Printf("sum on %v is %d\n", s, sum)
+
 }
 
-
-func fact(x int) int{
+func fact(x int) int {
 	if x == 1 {
 		return x
 	} else {
 		return x * fact(x-1)
+	}
+}
+
+func sum (s []int) int {
+	if(len(s) == 0) {
+		return 0
+	}else {
+		return s[len(s)-1] + sum(s[0:len(s)-1])
 	}
 }
